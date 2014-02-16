b3dfb16780e2cde3732e61f60bf1c3f57a9103b0
vwilmet <33bubulle33@gmail.com>
2014-02-16 14:37:39 +0100

    Modification des noms de fichiers et supression du fichier FileManagerInterface

--- FileManagerPdp/src/com/manager/FilesManager.java
+++ FileManagerPdp/src/com/manager/FilesManager.java
@@ -1,52 +1,42 @@
 package com.manager;
 
-import java.util.ArrayList;
-
-public class FilesManager implements FileManagerInterface{
+/**
+ * Interface de gestion du controleur de fichier
+ * @author <ul><li>Vincent Wilmet</li><li>Morgane Badré</li>
+ * @version 1.0
+ */
+public interface FilesManager {
 	
-	ArrayList<MNBVFile> files = new ArrayList<MNBVFile>();
-
-	public FilesManager() {
-		
-	}
+	/**
+	 * Méthode initialisant le module :<ul>
+	 * <li> préparation du fichier de sauvegarde automatique </li>
+	 * <li> préparation du tableau de gestion de plusieurs fichiers </li>
+	 * @param enableVerification Un booléen qui, si passé à true, autorise la vérification de l'intérité du fichier
+	 */
+	public void init(boolean enableVerification);
 	
-	private MNBVFile createFile(String name){
-		
-		return null;
-	}
+	/**
+	 * Voir la documentation {@link com.manager.FilesManager#init}
+	 * @param autosaveFilename Le nom du fichier pour les sauvegardes automatiques. Par défault le nom est othello_autosave-{date:AAAA-MM-JJ-HH-MM-SS}.mnbv
+	 * @param enableVerification Un booléen qui, si passé à true, autorise la vérification de l'intérité du fichier
+	 */
+	public void init(String autosaveFilename, boolean enableVerification);
 	
+	/**
+	 * Voir la documentation {@link com.manager.FilesManager#init}
+	 * @param saveFilename Le nom du fichier de sauvegarde générale si on ne veut pas le préciser à chaque sauvegarde utilisateur.
+	 * Par défault le nom est othello_save-{date:AAAA-MM-JJ-HH-MM-SS}.mnbv
+	 * @param autosaveFilename Le nom du fichier pour les sauvegardes automatiques. Par défault le nom est othello_autosave-{date:AAAA-MM-JJ-HH-MM-SS}.mnbv
+	 * @param enableVerification Un booléen qui, si passé à true, autorise la vérification de l'intérité du fichier
+	 */
+	public void init(String saveFilename, String autosaveFilename, boolean enableVerification);
 	
-	@Override
-	public boolean save(String data) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean save(String name, String data) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean autoSave(String data) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public String load(String name) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void enableVerification() {
-		// TODO Auto-generated method stub
-		
-	}
+	public boolean save(String data);
+	public boolean save(String name, String data);
+	public boolean autoSave(String data);
 	
+	public String load(String name);
 	
-	
-	
+	public void enableVerification();
+	public void disableVerification();
 }
---
+++ FileManagerPdp/src/com/manager/FilesManagerImpl.java
@@ -0,0 +1,77 @@
+package com.manager;
+
+import java.util.ArrayList;
+
+public class FilesManagerImpl implements FilesManager{
+	
+	ArrayList<MNBVFile> files = new ArrayList<MNBVFile>();
+
+	public FilesManagerImpl() {
+		
+	}
+	
+	private MNBVFile createFile(String name){
+		
+		return null;
+	}
+	
+	
+	@Override
+	public boolean save(String data) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean save(String name, String data) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean autoSave(String data) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String load(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void enableVerification() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(boolean enableVerification) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(String autosaveFilename, boolean enableVerification) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(String saveFilename, String autosaveFilename,
+			boolean enableVerification) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void disableVerification() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+}

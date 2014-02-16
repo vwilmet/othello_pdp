1fd1593788e5b34fbcdcf6dc6a19a3395c315f3b
vwilmet <33bubulle33@gmail.com>
2014-02-16 19:28:20 +0100

    Ajout des commentaires

--- FileManagerPdp/src/utils/FileDateManager.java
+++ FileManagerPdp/src/utils/FileDateManager.java
@@ -3,8 +3,26 @@ package utils;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * Classe qui permet de générer la date formater spécialement pour le nom de fichier
+ * @author <ul><li>Vincent Wilmet</li><li>Morgane Badré</li>
+ * @version 1.0
+ */
 public class FileDateManager {
 	
+	/**
+	 * Méthode retournant la date sous format AAAA-MM-DD-HH-MM-SS avec : 
+	 * <ul>
+	 * <li>AAAA : Année -> 2014 </li>
+	 * <li>MM : Mois -> 01 </li>
+	 * <li>DD : jour -> 12 </li>
+	 * <li>HH : heure -> 24 </li>
+	 * <li>MM : minute -> 36 </li>
+	 * <li>SS : seconde -> 38 </li>
+	 * <li>Exemple : 2014-02-16-19-27-38</li>
+	 * </ul>
+	 * @return la date + heure en String
+	 */
 	public static String getDateFormatAAAAMMJJHHMMSS(){
 		return new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(new Date());
 	}

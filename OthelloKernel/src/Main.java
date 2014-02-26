cb33110ee9c4d402bc9da88c202512a45b311255
bletourn <benjamin.letourneau13@gmail.com>
2014-02-26 10:45:48 +0100

    correction main

--- OthelloKernel/src/Main.java
+++ OthelloKernel/src/Main.java
@@ -1,10 +1,14 @@
 import java.awt.Color;
 
+import utils.Application;
+
 import com.model.factory.FactoryProducer;
 import com.model.factory.interfaces.PieceFactory;
 import com.model.factory.interfaces.PlayerFactory;
 import com.model.piece.Piece;
 import com.model.player.Player;
+
+
 
 /**
  * @author Benjamin Letourneau
@@ -13,14 +17,15 @@ import com.model.player.Player;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Application app = Application.getInstance();
+		
 		System.out.println("Othello Kernel");
 		
 		System.out.println("Test de la factory : ");
 		
 		PieceFactory pieceFactory = FactoryProducer.getPieceFacory();
 		Piece p1 = pieceFactory.getWhitePiece(10, 10, 1, 1);
-		
+
 		PlayerFactory playerFactory = FactoryProducer.getPlayerFacory();
 		Player joueur1 = playerFactory.getHumanPlayer("batmann33", Color.white);
 		Player joueur2 = playerFactory.getMachinePlayer("John", Color.black);

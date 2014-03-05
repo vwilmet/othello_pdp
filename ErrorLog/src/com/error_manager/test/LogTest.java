ce3752444b9770d0bef9f2294160c1450425d5ca
vwilmet <33bubulle33@gmail.com>
2014-03-05 16:18:03 +0100

    Ajout des premiers tests

---
+++ ErrorLog/src/com/error_manager/test/LogTest.java
@@ -0,0 +1,19 @@
+package com.error_manager.test;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class LogTest extends TestCase {
+
+	@Test
+	public void testError() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testReset() {
+		fail("Not yet implemented");
+	}
+
+}

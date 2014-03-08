f0a32514818f6cf865ba4e5159796057534ebdbc
nyvon <nicolasyvon33@gmail.com>
2014-03-08 23:44:02 +0100

    Changement des structures utilisés par les IA
    Passage à une énumération à la place du design pattern state (permet de
    gagner une profondeur sur l’alpha-beta basique pour du 5x5)

--- ArtificialIntelligence/src/com/ai/impl/BruteForceAI.java
+++ ArtificialIntelligence/src/com/ai/impl/BruteForceAI.java
@@ -1,24 +1,19 @@
 package com.ai.impl;
 
 import java.awt.Point;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.Stack;
 
 import com.ai.ArtificialIntelligence;
 import com.board.Board;
-import com.board.Box;
-import com.board.BoxImpl;
-import com.tree.Node;
-import com.tree.Tree;
+import com.tree.NodeMove;
+import com.tree.TreeMove;
 import com.utils.WrongPlayablePositionException;
 
 public class BruteForceAI implements ArtificialIntelligence {
 
-	Box[][] board;
-	Tree<Point> tree;
+	TreeMove<Point> tree;
 	Set<Point> whitePiece;
 	Set<Point> blackPiece;
 	Integer boardWidth;
@@ -26,7 +21,8 @@ public class BruteForceAI implements ArtificialIntelligence {
 	private Set<Point> borderLine;
 	List<Integer> scoreArray;
 	Integer call;
-
+	Board initBoard;
+	
 	@Override
 	public Point nextMove(Integer player) {
 		// TODO Auto-generated method stub
@@ -48,40 +44,26 @@ public class BruteForceAI implements ArtificialIntelligence {
 	@Override
 	public Boolean initialize(Set<Point> whitePiece, Set<Point> blackPiece,
 			Integer boardWidth, Integer boardHeight) {
-		this.whitePiece = whitePiece;
-		this.blackPiece = blackPiece;
-		this.boardWidth = boardWidth;
-		this.boardHeight = boardHeight;
-		board = new Box[boardWidth][boardHeight];
-		for(Integer i = 0; i < this.boardWidth; i ++)
-			for(Integer j = 0; j < this.boardHeight; j++){
-				board[i][j] = new BoxImpl();
-				if(whitePiece.contains(new Point(i,j)))
-					board[i][j].putP1Piece();
-				else if(blackPiece.contains(new Point(i,j)))
-					board[i][j].putP2Piece();
-				else
-					board[i][j].removePiece();
-			}
-		tree = new Tree<Point>();
-		tree.setRootElement(new Node<Point>(new Point(-1,-1), 1,whitePiece,blackPiece,board,boardWidth,boardHeight));
+		initBoard = new Board(boardWidth, boardHeight, whitePiece, blackPiece);
+		tree = new TreeMove<Point>();
+		tree.setRootElement(new NodeMove<Point>(new Point(-1,-1),1,initBoard));
 		tree.setSentinel(tree.getRootElement());
-		scoreArray = new ArrayList<Integer>();
 		call = 0;
-		//Integer finalScore = miniMax(13, tree.getSentinel());
+		//Integer finalScore = miniMax(8, tree.getSentinel());
 		Integer alpha = Integer.MIN_VALUE;
 		Integer beta = Integer.MAX_VALUE;
-		Integer finalScore = alphaBeta(8, tree.getSentinel(), alpha, beta);
+		Integer finalScore = alphaBeta(12, tree.getSentinel(), alpha, beta);
 		System.out.println("Call : " + call);
 		showBestMoveParty();
 		return true;
 	}
 
-	public Integer miniMax(Integer depth, Node<Point> node){
-		Stack<Point> playablePosition = calculatePlayablePosition(node.getBoard(),node.getWhitePiece(),node.getBlackPiece(),node.getPlayer());
+	public Integer miniMax(Integer depth, NodeMove<Point> node){
+		Stack<Point> playablePosition = node.calculatePlayablePosition();
 		call += 1;
 		Integer i = 0;
 		Integer bestScore;
+		Integer opponent = node.getPlayer()%2+1;
 		if(node.getPlayer() == 1)
 			bestScore = Integer.MIN_VALUE;
 		else
@@ -90,15 +72,17 @@ public class BruteForceAI implements ArtificialIntelligence {
 		if(!playablePosition.isEmpty() && depth > 0){ 
 			while(!playablePosition.isEmpty() && depth > 0){
 				Point p = playablePosition.pop();
-				Node<Point> n = calculateTurnResult(p, node.getCopyOfBoard(), node.getCopyOfWhitePiece(), node.getCopyOfBlackPiece(), node.getPlayer());
+				NodeMove<Point> n = new NodeMove<Point>(p,opponent,new Board(node.getBoard())); 
+				n.calculateTurnResult();
+						//calculateTurnResult(p, node.getCopyOfBoard(), node.getCopyOfWhitePiece(), node.getCopyOfBlackPiece(), node.getPlayer());
 				node.addChild(n);
 
 				Integer score = miniMax(depth-1, n);
-				if(node.player == 1 && score > bestScore){
+				if(node.getPlayer() == 1 && score > bestScore){
 					bestScore = score;
 					node.setBestMove(n.getData());
 				}
-				else if(node.player == 2 && score < bestScore){
+				else if(node.getPlayer() == 2 && score < bestScore){
 					bestScore = score;
 					node.setBestMove(n.getData());
 				}
@@ -106,36 +90,38 @@ public class BruteForceAI implements ArtificialIntelligence {
 
 			}
 		}
-		else if(!calculatePlayablePosition(node.getBoard(),node.getWhitePiece(),node.getBlackPiece(),node.getPlayer()%2+1).isEmpty() && depth > 0){
-			node.setPlayer(node.getPlayer()%2 + 1);
+		else if(!node.getBoard().calculatePlayablePosition(opponent).isEmpty() && depth > 0){
+			node.setPlayer(opponent);
 			bestScore = miniMax(depth, node);
 		}
 		else{
-			bestScore = node.getNbWhite();
+			bestScore = node.getBoard().getNbWhitePiece();
 		}
 		return bestScore;
 
 	}
 	
-	public Integer alphaBeta(Integer depth, Node<Point> node, Integer alpha, Integer beta){
-		Stack<Point> playablePosition = calculatePlayablePosition(node.getBoard(),node.getWhitePiece(),node.getBlackPiece(),node.getPlayer());
+	public Integer alphaBeta(Integer depth, NodeMove<Point> node, Integer alpha, Integer beta){
+		Stack<Point> playablePosition = node.calculatePlayablePosition();
 		Integer i = 0;
 		Integer bestScore=0;
 		call += 1;
+		Integer opponent = node.getPlayer()%2+1;
 		if(!playablePosition.isEmpty() && depth > 0){ 
 			while(!playablePosition.isEmpty() && depth > 0){
 				Point p = playablePosition.pop();
-				Node<Point> n = calculateTurnResult(p, node.getCopyOfBoard(), node.getCopyOfWhitePiece(), node.getCopyOfBlackPiece(), node.getPlayer());
+				NodeMove<Point> n = new NodeMove<Point>(p,opponent,new Board(node.getBoard())); 
+				n.calculateTurnResult();
 				node.addChild(n);
 				Integer score = alphaBeta(depth-1, n, alpha, beta);
-				if(node.player == 1 && score > alpha){
+				if(node.getPlayer() == 1 && score > alpha){
 					alpha = score;
 					bestScore = alpha;
 					node.setBestMove(n.getData());
 					if(alpha>=beta)
 						break;
 				}
-				else if(node.player == 2 && score < beta){
+				else if(node.getPlayer() == 2 && score < beta){
 					beta = score;
 					bestScore = beta;
 					node.setBestMove(n.getData());
@@ -149,20 +135,20 @@ public class BruteForceAI implements ArtificialIntelligence {
 
 			}
 		}
-		else if(!calculatePlayablePosition(node.getBoard(),node.getWhitePiece(),node.getBlackPiece(),node.getPlayer()%2+1).isEmpty() && depth > 0){
+		else if(!node.getBoard().calculatePlayablePosition(opponent).isEmpty() && depth > 0){
 			node.setPlayer(node.getPlayer()%2 + 1);
 			bestScore = alphaBeta(depth, node, alpha, beta);
 		}
 		else{
-			bestScore = node.getNbWhite();
+			bestScore = node.getBoard().getNbWhitePiece();
 		}
 		return bestScore;
 
 	}
 	
-	public Node<Point> findNodeFromMove(Node<Point> node, Point p){
-		Node<Point> n = null;
-		for(Node<Point> child : node.getChildren()){
+	public NodeMove<Point> findNodeFromMove(NodeMove<Point> node, Point p){
+		NodeMove<Point> n = null;
+		for(NodeMove<Point> child : node.getChildren()){
 			if(child.getData().equals(p)){
 				n = child;
 				break;
@@ -172,15 +158,15 @@ public class BruteForceAI implements ArtificialIntelligence {
 	}
 	
 	public void showBestMoveParty(){
-		Node<Point> s = tree.getRootElement();
+		NodeMove<Point> s = tree.getRootElement();
 		Point p = s.getBestMove();
 		while(p != null){
-			printBoard(s.board);
+			System.out.println(s.printBoard());
 			System.out.println("BestMove : " + p.toString());
 			s = findNodeFromMove(s, p);
 			p = s.getBestMove();
 		}
-		printBoard(s.board);
+		System.out.println(s.printBoard());
 		System.out.println("End of game!");
 			
 	}
@@ -198,176 +184,7 @@ public class BruteForceAI implements ArtificialIntelligence {
 		return null;
 	}
 
-	public Set<Point> calculateBorderLine(Box[][] nodeBoard, Set<Point> nodeWhitePiece, Set<Point> nodeBlackPiece, Integer player){
-		Set<Point> borderLine = new HashSet<Point>();
-		if(player == 1){
-			for(Point black : nodeBlackPiece){
-				for(int x = - 1; x < 2; x++){
-					for(int y = -1; y < 2; y++){
-						if(black.x + x >= 0 && black.x + x < this.boardWidth && black.y + y >= 0 && black.y + y < this.boardHeight && !(x == 0 && y == 0)){
-							if(nodeBoard[x + black.x][y + black.y].isEmpty()){
-								borderLine.add(new Point(x + black.x, y + black.y));
-							}
-						}
 
-					}
-				}
 
-			}
-		}
-		else{
-			for(Point white : nodeWhitePiece){
-				for(int x = - 1; x < 2; x++){
-					for(int y = -1; y < 2; y++){
-						if(white.x + x >= 0 && white.x + x < this.boardWidth && white.y + y >= 0 && white.y + y < this.boardHeight && !(x == 0 && y == 0)){
-							if(nodeBoard[x + white.x][y + white.y].isEmpty()){
-								borderLine.add(new Point(x + white.x, y + white.y));
-							}
-						}
-
-					}
-				}
-
-			}
-		}
-		return borderLine;
-	}
-
-	public Stack<Point> calculatePlayablePosition(Box[][] nodeBoard, Set<Point> boardWhitePiece, Set<Point> boardBlackPiece, Integer player){
-		Set<Point> nodeBorderline = calculateBorderLine(nodeBoard, boardWhitePiece, boardBlackPiece, player);
-		Stack<Point> possiblePosition = new Stack<Point>();
-		for(Point p : nodeBorderline){
-			for(int x = - 1; x < 2; x++){
-				for(int y = -1; y < 2; y++){
-					if((p.x+x) < boardWidth && (p.x + x) >= 0 && (p.y + y) < boardHeight && (p.y+ y) >= 0 && !(x == 0 && y == 0)){
-						if(!nodeBoard[p.x + x][p.y + y].isPlayer(player) && !nodeBoard[p.x+x][p.y+y].isEmpty()){
-							Integer i = p.x + x + x;
-							Integer j = p.y + y + y;
-							Boolean playable = false;
-							while(i < boardWidth && i >= 0 && j < boardHeight && j >= 0 && playable == false){
-								if(!nodeBoard[i][j].isPlayer(player) && !nodeBoard[i][j].isEmpty()){ // teste s'il y a un pion et si c'est un pion adverse
-									i += x;
-									j += y;
-								}
-								else if(nodeBoard[i][j].isPlayer(player)) //teste le pion du joueur qui va "encadrer le coup" 
-									playable = true;
-								else if(nodeBoard[i][j].isEmpty())
-									break;
-							}
-							if(playable && !possiblePosition.contains(p)){
-								possiblePosition.push(p);
-							}
-						}
-					}
-				}
-			}
-		}
-		return possiblePosition;
-
-	}
-	public Node<Point> calculateTurnResult(Point position, Box[][] newBoard, Set<Point> whitePiece, Set<Point> blackPiece, Integer player){
-		if(player == 1){
-			whitePiece.add(new Point(position.x,position.y));
-			newBoard[position.x][position.y].putP1Piece();
-			Set<Point> tmp = new HashSet<Point>();
-			for(int x = - 1; x < 2; x++){
-				for(int y = -1; y < 2; y++){
-					if((position.x+x) < boardWidth && (position.x + x) >= 0 && (position.y + y) < boardHeight && (position.y+ y) >= 0 && !(x == 0 && y == 0)){
-						if(newBoard[position.x + x][position.y + y].isP2Piece()){
-							tmp.clear();
-							tmp.add(new Point(position.x+x,position.y+y));
-							Integer i = position.x + x + x;
-							Integer j = position.y + y + y;
-							while(i < boardWidth && i >= 0 && j < boardHeight && j >= 0){
-								if(newBoard[i][j].isP2Piece()){
-									tmp.add(new Point(i,j));
-									i += x;
-									j += y;
-								}
-								else if (newBoard[i][j].isP1Piece()){
-									whitePiece.addAll(tmp);
-									for(Point p : tmp)
-										newBoard[p.x][p.y].turnPiece();
-									break;
-								}
-								else if(newBoard[i][j].isEmpty())
-									break;
-							}
-						}
-					}
-				}
-			}
-			blackPiece.removeAll(whitePiece);
-		}
-		else if(player == 2){
-			newBoard[position.x][position.y].putP2Piece();
-			blackPiece.add(new Point(position.x,position.y));
-			Set<Point> tmp = new HashSet<Point>();
-			for(int x = - 1; x < 2; x++){
-				for(int y = -1; y < 2; y++){
-					if((position.x+x) < boardWidth && (position.x + x) >= 0 && (position.y + y) < boardHeight && (position.y+ y) >= 0 && !(x == 0 && y == 0)){
-						if(newBoard[position.x + x][position.y + y].isP1Piece()){
-							tmp.clear();
-							tmp.add(new Point(position.x+x,position.y+y));
-							Integer i = position.x + x + x;
-							Integer j = position.y + y + y;
-							Boolean stop = false;
-							while(i < boardWidth && i >= 0 && j < boardHeight && j >= 0 && stop == false){
-								if(newBoard[i][j].isP1Piece()){
-									tmp.add(new Point(i,j));
-									i += x;
-									j += y;
-								}
-								else if (newBoard[i][j].isP2Piece()){
-									blackPiece.addAll(tmp);
-									for(Point p : tmp){
-										newBoard[p.x][p.y].turnPiece();
-									}
-									break;
-								}
-								else if(newBoard[i][j].isEmpty())
-									break;
-							}
-
-						}
-					}
-				}
-			}
-			whitePiece.removeAll(blackPiece); 
-		}
-		return new Node<Point>(position, player%2+1, whitePiece, blackPiece, newBoard, boardWidth, boardHeight);
-	}
-
-	public Box[][] copyOfBoard(Box[][] oldBoard){
-		Box[][] newBoard = new Box[this.boardWidth][this.boardHeight];
-		for(int i = 0; i < this.boardWidth;i++)
-			for(int j = 0; j < this.boardHeight; j++){
-				newBoard[i][j] = new BoxImpl();
-				newBoard[i][j].setState(oldBoard[i][j].getState());
-			}
-
-		return newBoard;
-	}
-
-	public void printBoard(Box[][] board){
-		for(int i = 0; i < this.boardWidth; i++)
-			System.out.print(" _");
-		for(int j = 0; j < this.boardHeight; j++){
-			System.out.println("");
-			System.out.print("|");
-			for(int i = 0; i < this.boardWidth; i++){
-				if(board[i][j].isP1Piece())
-					System.out.print("1|");
-				else if(board[i][j].isP2Piece())
-					System.out.print("2|");
-				else
-					System.out.print(" |");
-			}
-			System.out.println("");
-			for(int i = 0; i < this.boardWidth; i++)
-				System.out.print(" _");
-		}
-		System.out.println("");
-
-	}
+	
 }
